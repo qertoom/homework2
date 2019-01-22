5a187d26890d574e0e5dcf68c6f2fbe99b689f2f
qertoom <46781956+qertoom@users.noreply.github.com>
2019-01-23 01:50:38 +0300

    Update main.js
    + неувиденный второй таск

--- main.js
+++ main.js
@@ -9,3 +9,28 @@ if ((age >=1) && (age <=17)){
 } else {
 	console.log('Enter correct age')
 }
+
+var array = [1,2,3,4,5,6,7,8,9];
+var i;
+
+for (i=1;i< (array.length + 1);i++){ 
+switch (i) {
+    case 1: 
+    console.log(i,'Разработчик');
+    break;
+    case 2:
+    case 3:
+    case 4:
+    console.log(i,'Разработчика');
+    break;
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    console.log(i, 'Разработчиков');
+    break;
+
+}
+}
+

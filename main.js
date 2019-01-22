4a0d77bf45e07d445a2f0dd1abbf10bb06b19dfe
qertoom <zvyagincev_01@mail.ru>
2019-01-22 17:48:33 +0300

    commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var age=40;
+
+if ((age >=1) && (age <=17)){
+	console.log('Ucheba');
+} else if ((age >= 18) && (age <= 64)){
+	console.log('Rabota');
+} else if ((age >= 65) && (age <=100)){
+	console.log('Pensia');
+} else {
+	console.log('Enter correct age')
+}
